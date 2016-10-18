03f529eb74992c3593f9296573f111edae557832
Jonathan Guthrie <jono@perfect.org>
2016-10-18 15:28:06 -0400

    Initial commit

---
+++ Sources/AuthenticationConfig.swift
@@ -0,0 +1,27 @@
+//
+//  AuthenticationConfig.swift
+//  PerfectTurnstileSQLite
+//
+//  Created by Jonathan Guthrie on 2016-10-18.
+//
+//
+
+public struct AuthenticationConfig {
+	public var inclusions = [String]()
+	public var exclusions = [String]()
+
+	public var denied: String?
+
+	public mutating func include(_ str: String) {
+		inclusions.append(str)
+	}
+	public mutating func include(_ arr: [String]) {
+		inclusions += arr
+	}
+	public mutating func exclude(_ str: String) {
+		exclusions.append(str)
+	}
+	public mutating func exclude(_ arr: [String]) {
+		exclusions += arr
+	}
+}
