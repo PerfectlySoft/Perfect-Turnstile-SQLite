3bfacaad3da08ca9373998ec192f451df33225d2
Jonathan Guthrie <jono@perfect.org>
2016-11-24 16:15:18 -0500

    Moving to using the struct

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
 	name: "PerfectTurnstileSQLite",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/SwiftORM/SQLite-StORM.git", majorVersion: 0, minor: 0),
+		.Package(url: "https://github.com/SwiftORM/SQLite-StORM.git", majorVersion: 0, minor: 1),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
 		.Package(url: "https://github.com/iamjono/SwiftString.git",majorVersion: 1, minor: 0),
