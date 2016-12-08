3f2de7bad282c1160bc415ce49ec8773162857d2
Jonathan Guthrie <jono@perfect.org>
2016-12-08 10:50:00 -0500

    update to use Turnstile-Perfect 2.0+

--- Package.swift
+++ Package.swift
@@ -1,33 +1,15 @@
-//
-//  Package.swift
-//  PerfectTurnstileSQLite
-//
-//  Created by Jonathan Guthrie on 2016-10-11.
-//	Copyright (C) 2015 PerfectlySoft, Inc.
-//
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Perfect.org open source project
-//
-// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
-// Licensed under Apache License v2.0
-//
-// See http://perfect.org/licensing.html for license information
-//
-//===----------------------------------------------------------------------===//
-//
-
+// Generated automatically by Perfect Assistant Application
+// Date: 2016-12-08 15:42:48 +0000
 import PackageDescription
-
 let package = Package(
-	name: "PerfectTurnstileSQLite",
-	targets: [],
-	dependencies: [
-		.Package(url: "https://github.com/SwiftORM/SQLite-StORM.git", majorVersion: 0, minor: 1),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/iamjono/SwiftString.git",majorVersion: 1, minor: 0),
-		.Package(url: "https://github.com/iamjono/SwiftRandom.git",majorVersion: 0, minor: 2),
-		.Package(url: "https://github.com/PerfectSideRepos/Turnstile-Perfect.git", majorVersion:1)
-	]
+    name: "PerfectTurnstileSQLite",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/SwiftORM/SQLite-StORM.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/PerfectSideRepos/Turnstile-Perfect.git", majorVersion: 2, minor: 0),
+    ]
 )
