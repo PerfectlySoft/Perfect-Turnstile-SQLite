ef071602fb8494195c18001c62196b8427d83947
Jonathan Guthrie <jono@perfect.org>
2017-02-17 14:16:56 -0500

    sight update to versioning

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-08 16:02:13 +0000
+// Date: 2017-02-17 19:12:16 +0000
 import PackageDescription
 let package = Package(
     name: "PerfectTurnstileSQLite",
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/SwiftORM/SQLite-StORM.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0, minor: 2),
